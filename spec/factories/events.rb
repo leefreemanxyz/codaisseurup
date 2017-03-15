b3b8f08ddf0bf0256c376c69ade9dbe6f38d2d4b
leefreemanxyz <lee.freeman1@gmail.com>
2017-03-15 11:15:26 +0100

    Install FactoryGirl, Faker and define factories

---
+++ spec/factories/events.rb
@@ -0,0 +1,23 @@
+FactoryGirl.define do
+  factory :event do
+    name  "Best party in the world"
+    description {Faker::Lorem.sentece(40)}
+    location {Faker::Address.city}
+    price {Faker::Commerce.price}
+    capacity  10
+    includes_food true
+    includes_drinks true
+    starts_at   {Faker::Date.between(10.days.from_now, 12.days.from_now)}
+    ends_at   {Faker::Date.between(12.days.from_now, 15.days.from_now)}
+    user    {build(:user)}
+  end
+
+
+trait :active do
+  active true
+end
+
+trait :inactive do
+  active false
+end
+end
