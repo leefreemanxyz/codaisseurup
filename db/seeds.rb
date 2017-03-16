5d3a4edcd6d2995f0f1b53d1c6472984e711e647
leefreemanxyz <lee.freeman1@gmail.com>
2017-03-16 11:13:04 +0100

    update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,24 @@
-require "factory_girl_rails"
+Category.delete_all
+  Event.delete_all
+  User.delete_all
+  miriam = User.create!(
+   email: "miriam@codaisseurup.com",
+   password: "abcd1234",
+ )
 
-Category.destroy_all
-  Event.destroy_all
-  User.destroy_all
-10.times do
-  FactoryGirl.create :event
-end
-
-
-
-
+ event = Event.create!(
+   name: "Monthly Yoga Weekend",
+   description: "Let's come together and practice some asanas together in the woods!",
+   location: "Amsterdam",
+   includes_food: false,
+   includes_drinks: true,
+   price: 10.00,
+   starts_at: 10.days.from_now,
+   ends_at: 12.days.from_now,
+   capacity: 100,
+   active: true,
+   user: miriam,
+ )
 
  Category.create!([
    { name: "Movements" },
@@ -22,3 +31,7 @@ end
    { name: "Photography" },
    { name: "Food & Drink" },
  ])
+
+ event = Event.find_by(name: "Monthly Yoga Weekend")
+ event.categories << Category.find_by(name: "Outdoors & Adventure")
+ event.categories << Category.find_by(name: "Sports & Fitness")
