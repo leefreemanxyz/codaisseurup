3865a742a8eee32d48943997a76ddfafa95fe3e0
leefreemanxyz <lee.freeman1@gmail.com>
2017-03-16 11:24:14 +0100

    updated erorr messages

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,36 +1,33 @@
 class Event < ApplicationRecord
-  belongs_to :user
-  has_and_belongs_to_many :categories
+    belongs_to :user
+    has_and_belongs_to_many :categories
 
-  validates :name, presence:true
-  validates :description, presence:true, length: {maximum:500}
-  validate :end_after_start
-  validate :starts_after_today
-  validates :starts_at, presence:true
-  validates :ends_at, presence:true
+    validates :name, presence: true
+    validates :description, presence: true, length: { maximum: 500 }
+    validate :end_after_start
+    validate :starts_after_today
+    validates :starts_at, presence: true
+    validates :ends_at, presence: true
 
-  def free?
-    price == 0
-  end
-
-  def self.order_by_price
-    order(:price)
-  end
-
-private
-  def starts_after_today
-      return if starts_at.blank?
-      if starts_at < DateTime.now
-        errors.add(:starts_at, "must be after today")
-      end
-
-  end
-
-  def end_after_start
-      return if ends_at.blank? || starts_at.blank?
-      if ends_at < starts_at
-      errors.add(:ends_at, "must be after the start date")
+    def free?
+        price == 0
     end
-  end
 
+    def self.order_by_price
+        order(:price)
+    end
+
+    private
+
+    def starts_after_today
+        return if starts_at.blank?
+        errors.add(:starts_at, 'must be after today') if starts_at < DateTime.now
+    end
+
+    def end_after_start
+        return if ends_at.blank? || starts_at.blank?
+        if ends_at < starts_at
+            errors.add(:ends_at, 'must be after the start date')
+        end
+    end
 end
