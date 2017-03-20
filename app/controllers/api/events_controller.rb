c030f3912b7477b101f6baf29e9709cbc20be71c
leefreemanxyz <lee.freeman1@gmail.com>
2017-03-20 16:52:33 +0100

    added apis for creating and updating events

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,9 +1,63 @@
 class Api::EventsController < ApplicationController
+  skip_before_filter :verify_authenticity_token
 
   def index
     render status:200, json:{
       events: Event.all
     }.to_json
   end
+
+  def show
+    event = Event.find(params[:id])
+
+    render status: 200, json: {
+      event: event
+    }.to_json
+  end
+
+  def create
+    event = Event.new(event_params)
+
+    if event.save
+      render status: 201, json:{
+        message: "Event successfully created!",
+        event: event
+      }.to_json
+    else
+      render status:422, json: {
+        errors:event.errors
+      }.to_json
+    end
+  end
+
+  def update
+    event = Event.find(params[:id])
+
+    if event.update(event_params)
+      render status: 202, json:{
+        message: "Event successfully updated!",
+        event: event
+      }.to_json
+    else
+      render status:422, json: {
+        message:"The room could not be updated",
+        errors:event.errors
+      }.to_json
+    end
+  end
+
+  def destroy
+    event = Event.find(params[:id])
+    event.destroy
+
+    render status:200, json:{
+      message:"Event deleted"
+    }.to_json
+  end
+
+  private
+     def event_params
+       params.require(:event).permit(:name, :description, :location, :includes_food, :includes_drinks, :price, :starts_at, :ends_at, :capacity, :active, category_ids: [])
+     end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     resources :events
+    resources :categories
   end
 end
